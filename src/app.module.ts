f463b68e5b8add270f3800ecb16c37d79a392fee
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-11-09 13:43:40 +0100

    Feat: Add X402 initial implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { X402Module } from './x402/x402.module';
+import { APP_GUARD } from '@nestjs/core';
+import { X402Guard } from './x402/guards/x402.guard';
 
 @Module({
-  imports: [],
+  imports: [X402Module],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: X402Guard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
