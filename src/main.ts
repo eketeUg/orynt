29ef3ffd34f1bdcb28b93b7b8dcbc182d33a9c5a
eketeUg <emmanuelugochukwuekete@gmail.com>
2025-11-09 17:17:19 +0100

    DOCs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  const NODE_ENV = process.env.NODE_ENV || 'development';
+
+  app.enableCors();
+
+  app.setGlobalPrefix('api/v1');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Orynt API')
+    .setDescription('API documentation for the Discreet application')
+    .setVersion('1.0')
+    .addServer(
+      NODE_ENV === 'production'
+        ? process.env.PROD_URL || ''
+        : `http://localhost:${PORT}`,
+      NODE_ENV === 'production'
+        ? 'Production Server'
+        : 'Local Development Server',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/v1/docs', app, document);
+
+  await app.listen(PORT);
 }
 bootstrap();
